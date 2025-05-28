1ebba97b8c72242499302b3f9f0373391527730a
tuco520 <arthurbatistamoura@gmail.com>
2025-05-28 17:07:35 -0300

    Add files via upload

---
+++ aula6-refatoração-indentação-concatenação.py
@@ -0,0 +1,71 @@
+#indentação em python - organizar o código
+
+# questão 1
+resultado =(4 * 6 /2)
+print ('resultado:', resultado)
+# questão 2
+resultado= (5+7)
+print('resultado:', resultado)
+# questão 3
+resultado= 3**4
+print ('resultado: ', resultado)
+# questão 4
+resultado= (8+12+15)/3
+print ('resultado:', resultado)
+# questão 5
+resultado= (2-10)/2
+print ('resultado:', resultado)
+#questão 6
+resultado= (27/3)+5
+print ('resultado:', resultado)
+
+
+#
+resultado = (17%4)
+print ('resultado:', resultado)
+#
+
+
+
+resultado = (9*3)*2
+print ("resultado: ", resultado)
+
+resultado= (81**0.5)
+print ('resultado:', resultado)
+
+resultado= (3*4)+20
+print ('resultado:', resultado)
+
+resultado= (15*2)-7
+print ('resultado:', resultado)
+
+resultado= 5**3
+print ('resultado:', resultado)
+
+resultado= (17+21+25)/3
+print ('resultado:', resultado)
+
+resultado= (11*2)+7
+print ('resultado:', resultado)
+
+resultado= ((3*8)-15)/2
+print ('resultado:', resultado)
+
+resultado= 2**10
+print ('resultado:', resultado)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
