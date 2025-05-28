4092f34124ba8658f6dcf88bce406f74a472cad3
tuco520 <arthurbatistamoura@gmail.com>
2025-05-28 17:51:37 -0300

    Add files via upload

---
+++ listas-em-python,py.py
@@ -0,0 +1,16 @@
+#lista vazia
+lista = []
+
+#lista preenchida
+lista_teste = [1,2,3,4,5,6,7,8,9,]
+
+print (lista_teste[4])
+lista= []
+
+lista.append(10)
+
+print (lista)
+
+
+lista.extend([10,20,30,40,50,100])
+print (lista)
